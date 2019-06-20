c4016757c2cf819636df0a9ef2314fa2576aab4f
Macarena Haspert <hmacarena@gmail.com>
2019-06-20 11:31:19 -0300

    List

--- List/List.cpp
+++ List/List.cpp
@@ -11,3 +11,68 @@ List::List()
 List::~List()
 {
 }
+
+void List::Next() {
+	if (current) {
+		current = current->next;
+	}
+}
+
+void List::First() {
+	current = first;
+}
+
+void List::Last() {
+	current = first;
+	if (!IsEmpty()) {
+		while (current->next) {
+			Next();
+		}
+	}
+
+}
+
+bool List::IsEmpty() {
+	return first == NULL;
+}
+
+void List::Insert(int v) {
+	pnode previous;
+
+	if (IsEmpty() || first->value > v) {
+		first = new Node(v, first);
+	}
+	else {
+		previous = first;
+
+		while (previous->next && previous->next->value <= v) {
+			previous = previous->next;
+		}
+
+		previous->next = new Node(v, previous->next);
+	}
+}
+
+void List::Delete(int v) {
+	pnode previous, node;
+
+	node = first;
+	previous = NULL;
+
+	while (node && node->value < v) {
+		previous = node;
+		node = node->next;
+	}
+
+	if (!node || node->value != v) {
+		return;
+	}
+	else { // delete node
+		if (!previous) { // first element
+			first = node->next;
+		}
+		else { // other
+			previous->next = node->next;
+		}
+	}
+}
--- List/List.h
+++ List/List.h
@@ -5,6 +5,13 @@ class List
 public:
 	List();
 	~List();
+
+	void Next();
+	void First();
+	void Last();
+	bool IsEmpty();
+	void Insert(int v);
+	void Delete(int v);
 private:
 	pnode first;
 	pnode current;
